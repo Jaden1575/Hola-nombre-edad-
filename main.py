1066285bde38c34a93600912dc91bef07b21cb79
Jaden1575 <jonseagal@gmail.com>
2021-10-26 00:25:29 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Entrada de datos
+
+nombre= input("¿ Cual es tu nombre ? ")
+edad= input ("¿ Cual es tu edad ? ")
+
+#Salida
+
+print(f"Me alegro de conocerte, bienvenido {nombre}, veo que tienes {edad} años ")
+
